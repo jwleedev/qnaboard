64d6fc41f4e586b7a646a6eb3cc7abd5d96105f0
jiu <jwlee5510@gmail.com>
2026-05-16 21:03:33 +0900

    초기 프로젝트 세팅 완료

---
+++ src/main/java/hello/qnaboard/QnaboardApplication.java
@@ -0,0 +1,13 @@
+package hello.qnaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/qnaboard/QnaboardApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.qnaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
